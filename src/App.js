020c7dade6c908f0fe6ff2379fe64edba87c2f70
= <=>
2020-04-04 01:49:43 +0400

    Lifting state up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Calculator from './Calculator';
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Calculator/>
+      </div>
+    );
+  }
+}
+
+export default App;
